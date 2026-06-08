6fb76cbcd963b0895d28d5d39251470c31646df5
Lazy-Code-X01 <lazycode686@gmail.com>
2026-06-08 02:05:34 +0100

    tes: make chnage to react for auto redolpy

--- react-vite/server.js
+++ react-vite/server.js
@@ -4,4 +4,4 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')));
-app.listen(PORT, () => console.log(`React app running on port ${PORT}`));
+app.listen(PORT, () => console.log(`React app running on port ${PORT} and edited by lazy 😸`));
